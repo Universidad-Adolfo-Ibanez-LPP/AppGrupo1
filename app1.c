e2d6911097088de0fe4f629cad0f7e037f8ee16e
Tomás Loyola <100977113+Tsloyola@users.noreply.github.com>
2022-04-03 11:50:18 -0400

    Update app1.c
    Parte para guardar archivo y menu principal.

--- app1.c
+++ app1.c
@@ -114,3 +114,84 @@ void printValues(libro libro_especifico[])
         }
     }
 }
+//Guardar archivo
+void GuardarArchivo(char path[]){
+
+    FILE *archivo;
+    char cadena[255];
+    int numero=100;
+    printf("\n");
+
+    archivo=fopen(path,"w");
+
+    if (archivo !=NULL){
+        for (int i = 0; i < tope; ++i)
+        {
+            if (libro_especifico[i].anio!=0)
+            {
+                if (i < arreglado-1)
+                {
+                    fprintf(archivo, "%s,%s,%d,%d,%s,%d,%s,%s",libro_especifico[i].titulo,libro_especifico[i].autor, libro_especifico[i].anio, libro_especifico[i].estante_numero,libro_especifico[i].estante_seccion, libro_especifico[i].piso, libro_especifico[i].edificio, libro_especifico[i].sede);
+                }
+                else
+                {
+                    fprintf(archivo, "%s,%s,%d,%d,%s,%d,%s,%s\n",libro_especifico[i].titulo,libro_especifico[i].autor, libro_especifico[i].anio, libro_especifico[i].estante_numero,libro_especifico[i].estante_seccion, libro_especifico[i].piso, libro_especifico[i].edificio, libro_especifico[i].sede);
+                }
+            }
+
+        }
+    }
+    fclose(archivo);
+    system( "\tread -n 1 -s -p \"Presione cualquier tecla para volver al menu...\"" );
+}
+
+//Menu principal que contiene la mayoria de procesos
+void menuprincipal()
+{
+    int opcion;
+    bool repetir = true;
+
+
+    do{
+            system("@cls||clear");
+
+            printf("\n");
+            printf("\n\n\t\t\tMenu\n");
+            printf("\n");
+            printf("\n\t0: Revisar inventario.\n");
+            printf("\t1: Agregar datos a la biblioteca.\n");
+            printf("\t2: Eliminar datos de la biblioteca.\n");
+            printf("\t3: Cambiar datos de la biblioteca.\n");
+            printf("\t4: Buscar libro de la biblioteca.\n");
+            printf("\t5: Salir.\n");
+
+            printf("\n");
+
+            printf("\tIngrese una opcion: ");
+            scanf("%d",&opcion);
+
+            switch(opcion){
+                case 0:
+                    printValues(libro_especifico);
+                    system( "read -n 1 -s -p \"Presione cualquier tecla para volver al menu...\"" );
+                    break;
+                case 1:
+                    agregar();
+                    break;
+                case 2:
+                    quitar();
+                    break;
+                case 3:
+                    cambiar();
+                    break;
+                case 4:
+                    buscar_libro();
+                    break;
+                case 5:
+                    repetir= false;
+                    break;
+
+
+            }
+    }while (repetir);
+}
