6adefe54e7d0a64ccff611228a28e56dadb72771
Andrés Guerra <100784908+AndrewWar29@users.noreply.github.com>
2022-04-03 22:13:37 -0400

    Update app1.c

--- app1.c
+++ app1.c
@@ -12,7 +12,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "libro.h"
 
 //funciones menu y submenus
 void menuprincipal();
@@ -24,6 +23,19 @@ void quitar();
 void cambiar();
 
 void buscar_libro();
+
+//Creamos el tipo de dato libro
+typedef struct libro
+{
+    char titulo[255];
+    char autor[255];
+    int anio;
+    int estante_numero;
+    char estante_seccion[255];
+    int piso;
+    char edificio[255];
+    char sede[255];
+}libro;
 
 
 //Se realiza un array de structs para manipular
