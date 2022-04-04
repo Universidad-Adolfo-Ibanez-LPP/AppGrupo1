2895fd3eb58d35c8b859c7eee44aede26216ce37
Andrés Guerra <100784908+AndrewWar29@users.noreply.github.com>
2022-04-03 21:35:46 -0400

    Agregando archivo .h
    Definición de struct Libro

---
+++ libro.h
@@ -0,0 +1,13 @@
+
+//Creamos el tipo de dato libro
+typedef struct libro
+{
+    char titulo[255];
+    char autor[255];
+    int anio;
+    int estante_numero;
+    char estante_seccion[255];
+    int piso;
+    char edificio[255];
+    char sede[255];
+}libro;
